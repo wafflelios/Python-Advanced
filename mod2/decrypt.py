2380ab8c7159a505b7fffe867e80ab923e1b1e85
wafflelios <107104736+wafflelios@users.noreply.github.com>
2023-03-07 00:46:57 +0500

    Update decrypt.py

--- mod2/decrypt.py
+++ mod2/decrypt.py
@@ -1,13 +1,17 @@
 import sys
 
-data = str(sys.stdin.read())[:-1].split('..')
-not_empty_index = 0
-for counter in range(0, len(data) - 1):
-    if data[counter + 1] != '' and data[counter] != '':
-        data[counter] = data[counter][:-1]
-        not_empty_index = counter
-    else:
-        data[not_empty_index] = data[not_empty_index][:-1]
-res = ''.join(data).replace('.', '')
-print(res)
 
+def decrypt():
+    data = str(sys.stdin.read())[:-1].split('..')
+    not_empty_index = 0
+    for counter in range(0, len(data) - 1):
+        if data[counter + 1] != '' and data[counter] != '':
+            data[counter] = data[counter][:-1]
+            not_empty_index = counter
+        else:
+            data[not_empty_index] = data[not_empty_index][:-1]
+    res = ''.join(data).replace('.', '')
+    print(res)
+
+
+decrypt()
