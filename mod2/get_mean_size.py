3b7f537b60b3d049926a8d0a036e881c138d86b8
wafflelios <107104736+wafflelios@users.noreply.github.com>
2023-03-06 20:54:27 +0500

    Add files via upload

---
+++ mod2/get_mean_size.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+data = sys.stdin.readlines()
+if data:
+    summa, counter = 0, 0
+    for file in data:
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        path = os.path.join(base_dir, file[:-1])
+        if os.path.isfile(path):
+            stats = os.stat(path)
+            summa += stats.st_size
+            counter += 1
+    print(f'Средний размер файла в каталоге: {summa/counter}')
+else:
+    print('Либо в каталоге нет файлов, либо не удается получить их размер.')
