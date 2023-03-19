274586f21668b0e9bcb334502aca71bb03c60962
wafflelios <107104736+wafflelios@users.noreply.github.com>
2023-03-19 13:44:09 +0500

    Add files via upload

---
+++ mod4/5. Текущие процессы.py
@@ -0,0 +1,19 @@
+import shlex
+import subprocess
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/ps', methods=['GET'])
+def ps():
+    args: list[str] = request.args.getlist('arg')
+    clean_args = [shlex.quote(arg) for arg in args]
+    command = shlex.split(f'ps {"".join(clean_args)}')
+    res = subprocess.run(command, capture_output=True)
+    return f'<pre>${res.stdout.decode()}</pre>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
