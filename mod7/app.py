163c54737bab4fef7511e8e39c630970f11f654a
wafflelios <107104736+wafflelios@users.noreply.github.com>
2023-03-29 16:28:04 +0500

    Add files via upload

---
+++ mod7/app.py
@@ -0,0 +1,11 @@
+from utils import calculator
+import logging
+
+logger = logging.getLogger('app')
+logging.basicConfig(level=logging.DEBUG)
+
+logger.info('The program has started.')
+num_1, operation, num_2 = input('Enter each character separated by space: ').split()
+num_1, num_2 = int(num_1), int(num_2)
+calculator(num_1, num_2, operation)
+logger.info('App finished work.')
