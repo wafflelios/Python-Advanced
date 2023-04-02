7d6ffb2d6dc49136c02bf4dd6eab91ce6c1946cb
wafflelios <107104736+wafflelios@users.noreply.github.com>
2023-04-02 12:07:17 +0500

    Update app.py

--- mod7/add_logging/app.py
+++ mod7/add_logging/app.py
@@ -5,6 +5,7 @@ logger = logging.getLogger('app')
 logging.basicConfig(level=logging.DEBUG)
 
 logger.info('The program has started.')
+logger.warning("Don't forget that you can't divide by zero!")
 num_1, operation, num_2 = input('Enter each character separated by space: ').split()
 num_1, num_2 = int(num_1), int(num_2)
 calculator(num_1, num_2, operation)
