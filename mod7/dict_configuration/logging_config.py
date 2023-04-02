16a8d4a9bfd01ad40fd9e1b037244795a5dbfbcf
wafflelios <107104736+wafflelios@users.noreply.github.com>
2023-04-02 12:44:58 +0500

    Create logging_config.py

---
+++ mod7/dict_configuration/logging_config.py
@@ -0,0 +1,34 @@
+from custom_level_handler import LayeredHandler
+
+dict_config = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'base': {
+            'format': '%(levelname)s | %(name)s | %(asctime)s | %(lineno)d | %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'base',
+            'stream': 'ext://sys.stdout'
+        },
+        'file': {
+            '()': LayeredHandler,
+            'level': 'DEBUG',
+            'formatter': 'base',
+        }
+    },
+    'loggers': {
+        'app_logger': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'file']
+        },
+        'utils_logger': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'file']
+        }
+    }
+}
